cd77a826c8aad7cfc7a9a4af7acdb6de617d2ea6
kangyongbong <rkdqhd11@hanmail.net>
2021-10-06 00:27:20 +0900

    feat : 컴퓨터 랜덤숫자 생성 서비스 로직 구현

---
+++ src/main/java/baseball/service/impl/RandomGeneratorServiceImpl.java
@@ -0,0 +1,42 @@
+package baseball.service.impl;
+
+import baseball.constant.InputDefinitionConstant;
+import baseball.service.RandomGeneratorService;
+import nextstep.utils.Randoms;
+
+public class RandomGeneratorServiceImpl implements RandomGeneratorService {
+
+    private StringBuffer randomNumber;
+
+    public RandomGeneratorServiceImpl() {
+        randomNumber = new StringBuffer();
+    }
+
+    @Override
+    public String generatorNumber() {
+        return initNumber();
+    }
+
+    @Override
+    public String initNumber() {
+        do {
+            int number = Randoms.pickNumberInRange(InputDefinitionConstant.NUM_MIN, InputDefinitionConstant.NUM_MAX);
+            validateNumber(number);
+        } while (validateSize());
+
+        return randomNumber.toString();
+    }
+
+    @Override
+    public void validateNumber(int number) {
+        if (randomNumber.length() > 0 && randomNumber.indexOf("" + number) < 0) {
+            randomNumber.append(number);
+        }
+    }
+
+    @Override
+    public boolean validateSize() {
+        return randomNumber.length() < InputDefinitionConstant.NUM_SIZE;
+    }
+
+}
