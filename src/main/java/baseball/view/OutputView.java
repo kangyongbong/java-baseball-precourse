36b079be4894531b4969e7e19054883a81c083be
kangyongbong <rkdqhd11@hanmail.net>
2021-10-06 00:30:46 +0900

    feat : 출력 구현

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,44 @@
+package baseball.view;
+
+import baseball.constant.ErrorMessageConstant;
+import baseball.constant.GameMessageConstant;
+
+public class OutputView {
+
+    public void strikeMsg(int strike) {
+        System.out.println(strike + GameMessageConstant.RESULT_MSG);
+    }
+
+    public void ballMsg(int ball) {
+        System.out.println(ball + GameMessageConstant.BALL_MSG);
+    }
+
+    public void nothingMsg() {
+        System.out.println(GameMessageConstant.NOTHING_MSG);
+    }
+
+    public void strikeAndBallMsg(int strike, int ball) {
+        System.out.println(strike + GameMessageConstant.RESULT_MSG + " " +ball + GameMessageConstant.BALL_MSG);
+    }
+
+    public void endMsg(){
+        System.out.println(GameMessageConstant.RESULT_MSG);
+        System.out.println(GameMessageConstant.CONTINUE_MSG);
+    }
+
+    public void continueErrorMsg() {
+        System.out.println(ErrorMessageConstant.CONTINUE_ERROR_MSG);
+    }
+
+    public void numberSizeErrorMsg() {
+        System.out.println(ErrorMessageConstant.NUM_ERROR_MSG);
+    }
+
+    public void regErrorMsg() {
+        System.out.println(ErrorMessageConstant.CHAR_ERROR_MSG);
+    }
+
+    public void emptyErrorMsg() {
+        System.out.println(ErrorMessageConstant.EMPTY_ERROR_MSG);
+    }
+}
