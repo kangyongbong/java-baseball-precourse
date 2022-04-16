b2fcd75518c4b80de29ff2e6145a4bc8217d9d5b
kangyongbong <rkdqhd11@hanmail.net>
2022-04-16 16:02:28 +0900

    feat : create method for views

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,19 @@
 package baseball.view;
 
+import baseball.constant.ViewMessage;
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public String inputNumber(){
+        System.out.print(ViewMessage.INPUT_NUMBER.getMessage());
+
+        Console.readLine();
+        return Console.readLine();
+    }
+
+    public String inputRestart(){
+        System.out.print(ViewMessage.RESTART.getMessage());
+        return Console.readLine();
+    }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,4 +1,24 @@
 package baseball.view;
 
+import baseball.constant.StateMessage;
+import baseball.constant.ViewMessage;
+
 public class OutputView {
+
+
+    public void outputBallCount(int ballCount) {
+        System.out.println(ballCount + StateMessage.BALL.getMessage());
+    }
+
+    public void outputStrikeCount(int strikeCount) {
+        System.out.println(strikeCount + StateMessage.STRIKE.getMessage());
+    }
+
+    public void outputNothing(){
+        System.out.println(StateMessage.NOTHING.getMessage());
+    }
+
+    public void outputResult() {
+        System.out.println(ViewMessage.RESULT_END.getMessage());
+    }
 }
