0e157ba2b8ec4dbf99f7acd0f27127496b9e3cc6
kangyongbong <rkdqhd11@hanmail.net>
2022-04-16 16:05:25 +0900

    refactor : modify result message

--- src/main/java/baseball/constant/ViewMessage.java
+++ src/main/java/baseball/constant/ViewMessage.java
@@ -3,7 +3,7 @@ package baseball.constant;
 public enum ViewMessage {
 
     INPUT_NUMBER("숫자를 입력해주세요 : ")
-    , RESULT_END("개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    , RESULT_END("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
     , RESTART("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
     ViewMessage(String message) {
